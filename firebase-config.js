611407c062a5d545b6073459a0733875118072af
Revolutionxxxxxxxxxxxx <137689604+Revolutionxxxxxxxxxxxx@users.noreply.github.com>
2026-06-29 00:15:38 +0100

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDvzVJFQ7TGxGQor5HPDOoA8yqT7pqA1tE",
+    authDomain: "desfem-studio.firebaseapp.com",
+    projectId: "desfem-studio",
+    storageBucket: "desfem-studio.firebasestorage.app",
+    messagingSenderId: "322642846511",
+    appId: "1:322642846511:web:b9b1e3c53bd311d5cfb7da",
+    measurementId: "G-0YL05L9T51"
+  };
